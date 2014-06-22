016bb75dd8e4975a2e832ef6d3901bab68f0de36
Salvatore Sorrentino <salvatore.sorrentino@live.com>
2014-06-22 12:52:18 +0200

    Added a short description

--- ui.R
+++ ui.R
@@ -3,7 +3,11 @@ shinyUI(
 pageWithSidebar(
   headerPanel("Motor Trend Car Road Tests"),
   sidebarPanel(
-    h3("Enter the data"),
+    h4('The data was extracted from the 1974 Motor Trend US magazine, and comprises fuel consumption and 10 aspects of automobile design and performance for 32 automobiles (1973–74 models).'),
+    h4('Select he number of cylinders and /or the maximum Horse Power and we shall show you the corresponding average  Miles/Gallon'),
+    
+	h3("Enter the data"),
+	
 	radioButtons("cyl", "Number of cylinders:",
              c("4" = "4",
                "6" = "6",
